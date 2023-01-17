9a6c94d7c5a2650c77b27494c73f43d96cd83fa8
RodrigoFantechi <rodrifante595@libero.it>
2023-01-17 16:52:04 +0100

    fix image on show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,12 +2,13 @@
 @section('content')
 <a class="btn btn-primary" href="{{route('admin.projects.index')}}" role="button"><i class="fas fa-angle-left fa-fw"></i></a>
 <div class="container text-center p-5 d-flex align-items-center flex-column">
-        <div class="tecnologie">
+        
         @if ($project->cover_image)
         <img src="{{asset('storage/' . $project->cover_image)}}" alt="">
         @else
         <div class="segnaposto"><h6>placeholder</h6></div>
         @endif
+        <div class="tecnologie">
         <h1 class="mt-4">{{$project->title}}</h1>
         <h3>{{$project->slug}}</h3>
         <h5>{{$project->description}}</h5>
